f90cf5cb8e45db891741df21091173563bf2f233
PopescuStefan1 <stefan.ro95@gmail.com>
2024-11-06 19:35:04 +0200

    Simplify using writeHead

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ import http from "http";
 const PORT = 5000;
 
 const server = http.createServer((req, res) => {
-  res.setHeader("Content-Type", "text/plain");
+  //   res.setHeader("Content-Type", "text/html");
+  //   res.statusCode = 404;
 
-  res.end("<h1>Hello World!</h1>");
+  res.writeHead(500, { "Content-Type": "application/json" });
+  res.end(JSON.stringify({ message: "Server Error" }));
 });
 
 server.listen(PORT, () => {
